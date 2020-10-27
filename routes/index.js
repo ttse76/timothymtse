e87f6d62d2de29819e1ebdb2ee31587b21e40749
ttse76 <timtse76@gmail.com>
2020-10-27 11:53:41 -0400

    adding projects through JSON instead of pug and did some reorganizing of files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let projects = require('../util/projects.json');
 
-/* GET home page. */
+//get page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Timothy Tse' });
+  res.render('index', { 
+    title: 'Timothy Tse',
+    projectNames: projects.projects,
+    projObj: projects
+  });
 });
 
 module.exports = router;
