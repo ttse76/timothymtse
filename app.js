4f8d18e17e44d9ca98c0385d550b13ae2bc63f3b
ttse76 <timtse76@gmail.com>
2020-10-27 14:51:13 -0400

    removed unused packages

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 
@@ -13,7 +12,6 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
