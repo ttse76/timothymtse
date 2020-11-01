32085a8444e4dbb28ccbc32c0d8b89cf1cc01817
ttse76 <timtse76@gmail.com>
2020-11-01 12:41:10 -0500

    redid contact section

--- public/js/page.js
+++ public/js/page.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
     let preloaderFadeOutTime = 500;
     function hidePreloader(){
         let preloader = $('.preload-wrapper');
-        preloader.delay(1500);
+        preloader.delay(1000);
         preloader.fadeOut(preloaderFadeOutTime);
     }
     hidePreloader();
+    $('#encryptedEmail').addClass('contact-link').html('<img class="contact-img" src="/img/gmail.png" >');
+    $('div').tooltip({
+        container: '.contact-source'
+     });
 });
