9808ec648fd8ce5bd7a80f9c1b80fb504806c7e0
Tim Tse <timtse76@gmail.com>
2021-04-05 19:09:12 -0400

    created form

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 let projects = require('../util/projects.json');
 
+const JSUtils = require('../util/JSUtils');
+const Connection = require('../secrets/Connection');
+//RS4ELnZVKQo6QlwpUiyvOCI2Tf5mwJO4XK6oIYl9
 //get page
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -9,6 +12,10 @@ router.get('/', function(req, res, next) {
     projectNames: projects.projects,
     projObj: projects
   });
+});
+
+router.get('/' + Connection, (req, res) => {
+  res.render('weddingform.pug');
 });
 
 module.exports = router;
