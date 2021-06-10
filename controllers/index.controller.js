b2d8e4c760d7f2d59b3769e799f22f77e09bc62b
Tim Tse <timtse76@gmail.com>
2021-06-10 16:44:31 -0400

    updated about

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,8 +1,10 @@
 const { projects } = require('../config/projects.json');
+const { about } = require('../config/about.json');
 
 exports.index = (req, res) => {
     return res.render('index', { 
         title: 'Timothy Tse',
+        about: about,
         projObj: projects
     });
 };
