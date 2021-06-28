63e8d62521fe33d7840293e9b8ec5bafc2be63aa
Tim Tse <timtse76@gmail.com>
2021-06-28 14:56:20 -0400

    consolidated configs

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,5 +1,11 @@
-const { projects } = require('../config/projects.json');
-const { about } = require('../config/about.json');
+/*const { about } = require('../config/about.json');
+const { projects } = require('../config/projects.json');*/
+const { 
+    about, 
+    projects, 
+    roles 
+} = require('../config/config.json'); 
+
 
 exports.index = (req, res) => {
     return res.render('index', { 
