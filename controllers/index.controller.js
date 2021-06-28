7f619b51539d1cea7049558d7c73cd3ed931b2e3
Tim Tse <timtse76@gmail.com>
2021-06-28 14:57:16 -0400

    added roles to controller

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,5 +1,3 @@
-/*const { about } = require('../config/about.json');
-const { projects } = require('../config/projects.json');*/
 const { 
     about, 
     projects, 
@@ -11,6 +9,7 @@ exports.index = (req, res) => {
     return res.render('index', { 
         title: 'Timothy Tse',
         about: about,
-        projObj: projects
+        projObj: projects,
+        roles: roles
     });
 };
