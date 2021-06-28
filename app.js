0f417aee8e324d5cf91bde7aaf2086829b0d1b34
Tim Tse <timtse76@gmail.com>
2021-06-28 14:30:54 -0400

    added logger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/router');
 
@@ -10,6 +11,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
